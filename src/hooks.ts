e778746d605fa220fe3a784df972c27f4fcd3c82
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 17:51:07 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -45,3 +45,37 @@ export const useDimension = () => {
         h
     }
 }
+
+const maxScale = (
+    scale : number,
+    i : number,
+    n : number
+) : number => Math.max(0, scale - i / n)
+
+const divideScale = (
+    scale : number, 
+    i : number, 
+    n : number 
+) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const size : number = Math.min(w, h) / 10 
+    const sc1 : number = divideScale(scale, 0, 4)
+    const sc2 : number = divideScale(scale, 1, 4)
+    const sc3 : number = divideScale(scale, 2, 4)
+    const sc4 : number = divideScale(scale, 3, 4)
+    const width = `${size + (w / 2 - size) * (sc1 - sc3)}px`
+    const height = `${size}px`
+    const left = '0px'
+    const top = `${h / 2 + (h / 2 - size) * (sc2 - sc4)}px`
+    const background = '#1A237E'
+    return {
+        position, 
+        width, 
+        height, 
+        left, 
+        top,
+        background 
+    }
+}
